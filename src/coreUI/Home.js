d3f29aa1810526a09ab16a7bffefceb51c3d161e
windows <windows@microsoft.com>
2022-12-29 17:00:04 +0530

    install bootstrap

--- src/coreUI/Home.js
+++ src/coreUI/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
 import './global.css';
-import About11 from '../components/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const a=500;
+const b=200;
+let c=0;
+let j="red";
+let x=50;
 
 function MycoreHomee()
 {
     const variable = "This is an exaple of JavaScript variable"
 
+    if(a>=1000)
+    {
+        c=a;
+    }
+    else{
+        c=b;
+    }
 
     return(
         <> 
@@ -15,7 +28,7 @@ function MycoreHomee()
                     <img src="/css3Logo.png" alt="Error" className="logo-img" />
                 </div>
                 <div className="company-name">
-                    <span>Company</span>
+                    <span>Company {variable}</span>
                 </div>
             </div>
             <ul className="nav">
@@ -26,6 +39,77 @@ function MycoreHomee()
                 <li>About</li>
             </ul>
         </nav>
+
+        <div className="abc">
+            <p>welcome {c}</p>
+            <h2 style={{color:'red'}}>inline css</h2>
+            <h2 style={{color:j,fontSize:`${x}px`}}>inline css</h2>
+            <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+        </div>
+
+
+        <div className="card">
+            <h5 className="card-header">Featured</h5>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+            <div className="card">
+            <h5 className="card-header">Featured</h5>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+            <div className="card">
+            <h5 className="card-header">Featured</h5>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+
+<div className="container-fluid">
+    <div className="row">
+        <div className="col-4">
+        <div className="card">
+            <h5 className="card-header">Featured</h5>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div className="col-4">
+        <div className="card">
+            <h5 className="card-header">Featured</h5>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div className="col-4">
+        <div className="card">
+            <h5 className="card-header">Featured</h5>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
         </>
         
     )
