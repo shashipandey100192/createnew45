58ec1a863bfbcb9370735d5fd0163a8f1f97354b
windows <windows@microsoft.com>
2023-01-02 17:05:24 +0530

    all react ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MycoreHomee from './coreUI/Home';
+import Bootstraphome from './bootstrapUI/Bootstraphome';
+import Reactbootstraphome from './reactbootstrapUI/Reactbootstraphome';
+import Metrialhome from './metrialUI/metrialhome';
 
 
 
@@ -8,6 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MycoreHomee />
+    <Bootstraphome />
+    <Reactbootstraphome />
+    <Metrialhome />
   </React.StrictMode>
 );
 
---
+++ src/metrialUI/metrialhome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+function Metrialhome() {
+  return (
+    <div>
+        <Stack spacing={2} direction="row">
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </Stack>
+    </div>
+  )
+}
+export default Metrialhome
