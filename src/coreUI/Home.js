3aa22d7547ae56c89c067aa29722f0100a36db04
TCABrij <exreply7878@gmail.com>
2023-01-03 15:32:15 +0530

    created Heading component

--- src/coreUI/Home.js
+++ src/coreUI/Home.js
@@ -1,94 +1,87 @@
 import React from "react";
-import './global.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const a=500;
-const b=200;
-let c=0;
-let j="red";
-let x=50;
+const a = 500;
+const b = 200;
+let c = 0;
+let j = "red";
+let x = 50;
 
-function MycoreHomee()
-{
-    const variable = "This is an exaple of JavaScript variable"
+function MycoreHomee() {
+  if (a >= 1000) {
+    c = a;
+  } else {
+    c = b;
+  }
 
-    if(a>=1000)
-    {
-        c=a;
-    }
-    else{
-        c=b;
-    }
-
-    return(
-        <> 
-        <nav>
-            <div className="company">
-                <div className="logo">
-                    <img src="/css3Logo.png" alt="Error" className="logo-img" />
-                </div>
-                <div className="company-name">
-                    <span>Company {variable}</span>
-                </div>
-            </div>
-            <ul className="nav">
-                <li className="active">Home</li>
-                <li>Services</li>
-                <li>Contacts</li>
-                <li>Documentation</li>
-                <li>About</li>
-            </ul>
-        </nav>
-
-        <div className="abc">
-            <p>welcome {c}</p>
-            <h2 style={{color:'red'}}>inline css</h2>
-            <h2 style={{color:j,fontSize:`${x}px`}}>inline css</h2>
-            <button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
+  return (
+    <>
+      <nav>
+        <div className="company">
+          <div className="logo">
+            <img src="/css3Logo.png" alt="Error" className="logo-img" />
+          </div>
+          <div className="company-name">
+            <span>CoreUI</span>
+          </div>
         </div>
+        <ul className="nav">
+          <li className="active">Home</li>
+          <li>Services</li>
+          <li>Contacts</li>
+          <li>Documentation</li>
+          <li>About</li>
+        </ul>
+      </nav>
 
+      <div className="abc">
+        <p>welcome {c}</p>
+        <h2 style={{ color: "red" }}>inline css</h2>
+        <h2 style={{ color: j, fontSize: `${x}px` }}>inline css</h2>
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+        <button type="button" class="btn btn-secondary">
+          Secondary
+        </button>
+      </div>
 
-        
-
-<div className="container-fluid">
-    <div className="row">
-        <div className="col-4">
-        <div className="card">
-            <h5 className="card-header">Featured</h5>
-            <div className="card-body">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-4">
+            <div className="card">
+              <h5 className="card-header">Featured</h5>
+              <div className="card-body">
                 <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
             </div>
-            </div>
-        </div>
-        <div className="col-4">
-        <div className="card">
-            <h5 className="card-header">Featured</h5>
-            <div className="card-body">
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <h5 className="card-header">Featured</h5>
+              <div className="card-body">
                 <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        <div className="col-4">
-        <div className="card">
-            <h5 className="card-header">Featured</h5>
-            <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-        </>
-        
-    )
+      </div>
+    </>
+  );
 }
 export default MycoreHomee;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import MycoreHomee from './coreUI/Home';
 import Bootstraphome from './bootstrapUI/Bootstraphome';
 import Reactbootstraphome from './reactbootstrapUI/Reactbootstraphome';
 import Metrialhome from './metrialUI/metrialhome';
+import Heading from './components/Heading';
 
 
 
@@ -11,8 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MycoreHomee />
+    <Heading content="Bootstrap Example" variant="primary"/>
     <Bootstraphome />
     <Reactbootstraphome />
+    <Heading content="MaterialUI Example" variant="danger"/>
     <Metrialhome />
   </React.StrictMode>
 );
