f2748e281418409513bffcd59f322d1c8667c5ae
windows <windows@microsoft.com>
2023-01-04 17:04:22 +0530

    use state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,13 @@ function Home() {
   return (
     <div>
         <ul className='mynav'>
-            <li><Link to="home">Home</Link></li>
-            <li><Link to="about">About</Link></li>
-            <li><Link to="contact">Contact</Link></li>
-            <li><Link to="service">Serivce</Link></li>
-            <li><Link to="other">Other</Link></li>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/service">Serivce</Link></li>
+            <li><Link to="/other">Other</Link></li>
         </ul>
-
+        
 
 
     </div>
