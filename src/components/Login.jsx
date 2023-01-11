8e78552268f52bf575332ae9cf4defe1f9ae61c5
BRIJ <exreply7878@gmail.com>
2023-01-11 18:18:43 +0530

    add login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import "./login.css"
 import Nav from './Nav'
-import { Link } from 'react-router-dom'
 
 export default function Login() {
     return (
         <>
             <Nav />
-            <main>
+            <form action='/home'>
                 <div className="container">
                     <h2 className="login-title">Login to your Account</h2>
                     <input type="text" placeholder="Enter username" id="uname" required />
                     <input type="text" placeholder="Enter password" id="pass" required />
-                    <Link to="/home"><button className='btn'>Login</button></Link>
+                    <button type='submit' className='btn'>Login</button>
                 </div>
-            </main>
+            </form>
         </>
     )
 }
